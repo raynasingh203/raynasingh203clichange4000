b7db0eee472a5285e2c761920f03e61aa25d5a3b
raynasingh203 <150432573+raynasingh203@users.noreply.github.com>
2024-04-25 16:02:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function redirectToIndex() {
+    // Redirect to index.html when the toggle button is clicked
+    window.location.href = 'index.html';
+}
---
+++ script2.js
@@ -0,0 +1,7 @@
+function redirectToIndex() {
+
+    window.location.href = 'index.html';
+}
+
+
+
